f34b3193330f25d99b2ce2638ff89670d50771e5
Ayisha Khan <160420762+Ayishaakhan786@users.noreply.github.com>
2025-12-31 00:37:51 +0500

    Update docusaurus.config.js

--- book/docusaurus.config.js
+++ book/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://physicalai-humanoid-robotics.github.io',
+  url: 'https://physical-ai-humanoid-robotics-3mrv.vercel.app',
   // Set /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
